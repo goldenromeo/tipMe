bf9d2cd3cecb4b6ba648d48011bc06872e21550c
rommel <rommel@steves-macbook-air.local>
2015-12-29 22:33:14 -0500

    It works fully now

--- Tip aplication/ViewController.swift
+++ Tip aplication/ViewController.swift
@@ -10,6 +10,73 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //tip label initialize
+    @IBOutlet weak var tipLabel: UITextField!
+    
+    
+    
+    func calTip(tipAmount:Double){
+        
+        //get value
+        var tipLabelText = tipLabel.text
+        
+        //calculate tip amount
+        let tipamount = Double(tipLabelText!)! * tipAmount
+        
+        //use prefor
+        tipLabelText = "\(tipamount)"
+        
+        
+        tipLabel.text = tipLabelText
+        
+    
+    }
+    
+    /**
+    * calculates 15 % tip
+    */
+    
+    @IBAction func set15PercentTip(sender: AnyObject) {
+        
+        
+        if !tipLabel.text!.isEmpty
+        {
+            calTip(1.15)
+            
+        }
+        
+    }//end 15%
+    
+    
+    /**
+    * calculates 20 % tip
+    */
+    @IBAction func set20PercentTip(sender: AnyObject) {
+     
+        
+        if !tipLabel.text!.isEmpty
+        {
+            calTip(1.20)
+        
+        }
+    }
+    
+    
+    /**
+     * calculates 30 % tip
+     */
+    @IBAction func set30PercentTip(sender: AnyObject) {
+        
+        if !tipLabel.text!.isEmpty
+        {
+            calTip(1.30)
+            
+        }
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
